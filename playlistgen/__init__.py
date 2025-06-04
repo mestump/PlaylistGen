0748fa8c522f6836ad6403391c4ceab398b5ce66
mestump <mestump@gmail.com>
2025-06-04 16:01:39 -0400

    Remove unused legacy modules

--- playlistgen/__init__.py
+++ playlistgen/__init__.py
@@ -12,7 +12,6 @@ from .pattern_analyzer import (
     vectorize_playlist,
 )
 
-from .playlist_generator import generate_candidates
 from .similarity import score_playlists
 from .feedback import load_feedback, save_feedback, update_feedback
 from .train_model import train_cluster_model
@@ -26,7 +25,6 @@ __all__ = [
     "analyze_playlists",
     "vectorize_playlists",
     "vectorize_playlist",
-    "generate_candidates",
     "score_playlists",
     "train_cluster_model",
     "load_feedback",
